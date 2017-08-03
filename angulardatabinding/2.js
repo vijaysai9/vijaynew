2e43a2dcd6269799a6f13652751d1d78cd051199
vijaysai9 <vijaysai.vankadaru@gmail.com>
2017-08-03 14:12:10 -0500

    Add files via upload

---
+++ angulardatabinding/2.js
@@ -0,0 +1,12 @@
+var app = angular.module("app", []);
+app.controller('emp', ['$scope', function($scope){
+    $scope.a = 10;
+    $scope.b = 20;
+ 
+    $scope.doDoubleAndSum = function(){
+        $scope.a = $scope.a * 2;
+        $scope.b = $scope.b * 2;
+        $scope.sum = parseInt($scope.a)
+                        + parseInt($scope.b);
+    };
+}])
